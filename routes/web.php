083ae945d90433017e3ea8aef3d63ad2fb536e13
renom2098 <renom2098@gmail.com>
2025-04-22 16:31:00 +0700

    install template login dan register bawaan laravel

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 route::get('/debug-sentry', function () {
     throw new Exception('My first Sentry error!');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
