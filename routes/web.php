33f64a75d65a2a3fdef6fc8ef8fe372a9710092c
renom2098 <renom2098@gmail.com>
2025-04-22 16:16:16 +0700

    menambahkan sentry bug tracking

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+route::get('/debug-sentry', function () {
+    throw new Exception('My first Sentry error!');
+});
