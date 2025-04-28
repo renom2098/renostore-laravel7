ba0d515d054c6a20249a650f6b38f5ff8f3c2bef
renom2098 <renom2098@gmail.com>
2025-04-28 14:19:37 +0700

    konversi template pt3

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,64 @@
-@extends('layouts.app')
+@extends('layouts.auth')
 
 @section('content')
-<div class="container">
+<div class="page-content page-auth" id="register">
+    <div class="section-store-auth" data-aos="fade-up">
+        <div class="container">
+        <div class="row align-item-center justify-content-center row-login">
+
+            <div class="col-lg-4">
+            <h2>Memulai untuk jual beli <br> dengan cara terbaru</h2>
+            <form class="mt-3">
+                <div class="form-group">
+                <label for="#">Full Name</label>
+                <input type="text" class="form-control is-valid" v-model="name" autofocus>
+                </div>
+                <div class="form-group">
+                <label for="#">Email Address</label>
+                <input type="email" class="form-control is-invalid" v-model="email">
+                </div>
+                <div class="form-group">
+                <label for="#">Password</label>
+                <input type="password" class="form-control">
+                </div>
+                <div class="form-group">
+                <label for="#">Store</label>
+                <p class="text-muted">
+                    Apakah anda juga ingin membuka toko?
+                </p>
+                <div class="custom-control custom-radio custom-control-inline">
+                    <input type="radio" class="custom-control-input" name="is_store_open" id="openStoreTrue"
+                    v-model="is_store_open" :value="true">
+                    <label for="openStoreTrue" class="custom-control-label">Iya, boleh</label>
+                </div>
+                <div class="custom-control custom-radio custom-control-inline">
+                    <input type="radio" class="custom-control-input" name="is_store_open" id="openStoreFalse"
+                    v-model="is_store_open" :value="false">
+                    <label for="openStoreFalse" class="custom-control-label">Enggak, makasih</label>
+                </div>
+                </div>
+                <div class="form-group" v-if="is_store_open">
+                <label for="#">Nama Toko</label>
+                <input type="text" class="form-control">
+                </div>
+                <div class="form-group" v-if="is_store_open">
+                <label for="#">Kategori</label>
+                <select name="category" class="form-control" id="">
+                <option value="#" disabled>Select Category</option>
+                </select>
+                </div>
+
+                <a href="dashboard.html" class="btn btn-success btn-block mt-4">Sign Up Now</a>
+                <a href="login.html" class="btn btn-signup btn-block mt-2">Back to Sign In</a>
+            </form>
+            </div>
+
+        </div>
+        </div>
+    </div>
+</div>
+
+<div class="container hide" style="display: none">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -75,3 +132,32 @@
     </div>
 </div>
 @endsection
+
+@push('addon-script')
+<script src="vendor/vue/vue.js"></script>
+<script src="https://unpkg.com/vue-toasted"></script>
+<script>
+Vue.use(Toasted)
+var register = new Vue({
+    el: '#register',
+    mounted() {
+    AOS.init();
+    this.$toasted.error(
+        "Maaf, tampaknya email sudah terdaftar pada sistem kami.",
+        {
+        position: "top-center",
+        className: "rounded",
+        duration: 1000
+        }
+    );
+    },
+    data: {
+    name: "Reno Mujiarto",
+    email: "renom2098@gmail.com",
+    password: "",
+    is_store_open: true,
+    store_name: "",
+    }
+});
+</script>
+@endpush
