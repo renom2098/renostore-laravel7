c9ae5e92c8a8471f65ff17227257024764be0bc3
renom2098 <renom2098@gmail.com>
2025-05-19 17:02:36 +0700

    CRUD gallery product pt1

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -30,7 +30,9 @@
         <div class="list-group list-group-flush">
           <a href="{{route('admin-dashboard')}}" class="list-group-item list-group-item-action">Dashboard</a>
 
-          <a href="{{route('product.index')}}" class="list-group-item list-group-item-action {{(request()->is('admin/product*')) ? 'active' : ''}}">Products</a>
+          <a href="{{route('product.index')}}" class="list-group-item list-group-item-action {{(request()->is('admin/product')) ? 'active' : ''}}">Products</a>
+
+          <a href="{{route('product-gallery.index')}}" class="list-group-item list-group-item-action {{(request()->is('admin/product-gallery*')) ? 'active' : ''}}">Gallery</a>
 
           <a href="{{route('category.index')}}" class="list-group-item list-group-item-action {{(request()->is('admin/category*')) ? 'active' : ''}}">Categories</a>
 
